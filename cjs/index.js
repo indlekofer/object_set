79c14732db41dfedbaf8fe87d0e1d3506b4c834e
Asjid Kalam <asjid.kalam@gmail.com>
2021-01-10 16:50:28 +0530

    fixed prototype pollution

--- cjs/index.js
+++ cjs/index.js
@@ -8,6 +8,9 @@ exports["default"] = void 0;
 var setIn = function setIn(obj, path, value) {
   if (path.length == 0 || typeof path.splice == 'undefined' || typeof path[0] == 'undefined') return value;
   var key = path.splice(0, 1);
+  if(key.includes('__proto__') || key.includes('constructor') || key.includes('prototype')){
+    return obj;
+  }
   if (typeof obj[key] == 'undefined') obj[key] = {};
   obj[key] = setIn(obj[key], path, value);
   return obj;
